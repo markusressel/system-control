3600bd1956f75472d247fd6decab79144cc53ce5
Markus Ressel <mail@markusressel.de>
2019-12-20 21:20:28 +0100

    added args requirement

--- cmd/setBrightness.go
+++ cmd/setBrightness.go
@@ -33,6 +33,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		p, err := strconv.Atoi(args[0])
 		if err != nil {

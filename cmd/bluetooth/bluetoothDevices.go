f553396a6e4e95d2496a9fd600a5407395139d07
Markus Ressel <mail@markusressel.de>
2024-09-28 19:40:42 +0200

    improve parsing

---
+++ cmd/bluetooth/bluetoothDevices.go
@@ -0,0 +1,80 @@
+/*
+ * system-control
+ * Copyright (c) 2019. Markus Ressel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package bluetooth
+
+import (
+	"fmt"
+	"github.com/markusressel/system-control/internal/bluetooth"
+	"github.com/spf13/cobra"
+	"strconv"
+)
+
+var filterConnected bool
+var filterPaired bool
+
+var bluetoothDevicesCmd = &cobra.Command{
+	Use:   "devices",
+	Short: "List all known Bluetooth Devices",
+	Long:  ``,
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		devices, err := bluetooth.GetBluetoothDevices()
+		if err != nil {
+			return err
+		}
+
+		for _, device := range devices {
+			if filterConnected && !device.Connected {
+				continue
+			}
+			if filterPaired && !device.Paired {
+				continue
+			}
+
+			fmt.Printf("Name: %v\n", device.Name)
+			fmt.Printf("Address: %v\n", device.Address)
+			fmt.Printf("Connected: %v\n", device.Connected)
+			fmt.Printf("Paired: %v\n", device.Paired)
+			fmt.Println()
+		}
+
+		return nil
+	},
+}
+
+func parseFlagAsBool(cmd *cobra.Command, flagName string, defaultValue bool) (flagValue bool, err error) {
+	connectedFlag := cmd.Flag(flagName)
+	flagValue = defaultValue
+	if connectedFlag != nil {
+		connectedFlagValue := connectedFlag.Value.String()
+		if connectedFlagValue != "" {
+			flagValue, err = strconv.ParseBool(connectedFlagValue)
+			if err != nil {
+				return flagValue, err
+			}
+		}
+	}
+	return flagValue, nil
+}
+
+func init() {
+	Command.AddCommand(bluetoothDevicesCmd)
+
+	bluetoothDevicesCmd.Flags().BoolVarP(&filterConnected, "connected", "c", false, "Filter by connected state")
+	bluetoothDevicesCmd.Flags().BoolVarP(&filterPaired, "paired", "p", false, "Filter by paired state")
+}
--- internal/bluetooth/common.go
+++ internal/bluetooth/common.go
@@ -1,16 +1,37 @@
 package bluetooth
 
 import (
+	"github.com/google/uuid"
 	"github.com/markusressel/system-control/internal/util"
+	"strconv"
 	"strings"
 )
 
-// BluetoothDevice represents a bluetooth device
-type BluetoothDevice struct {
+// simpleDeviceInfo is a simple representation of a known bluetooth device and only used internally.
+// BluetoothDevice is a much more detailed representation.
+type simpleDeviceInfo struct {
 	// Name of the device
 	Name string
 	// Address MAC address of the device
 	Address string
+}
+
+// simpleDeviceInfo represents a bluetooth device
+
+type BluetoothDevice struct {
+	Name              string // LG-TONE-FP9
+	Address           string // B8:F8:BE:13:A4:72
+	Alias             string // LG-TONE-FP9
+	Class             string // 0x00240404 (2360324)
+	Icon              string // audio-headset
+	Paired            bool   // yes
+	Bonded            bool   // yes
+	Trusted           bool   // yes
+	Blocked           bool   // no
+	Connected         bool   // yes
+	LegacyPairing     bool   // no
+	UUIDs             map[string]uuid.UUID
+	BatteryPercentage *int64 // 0x4b (75)
 }
 
 // PairBluetoothDevice pairs a bluetooth device with the system
@@ -62,12 +83,56 @@ func GetBluetoothDevices() ([]BluetoothDevice, error) {
 		return nil, err
 	}
 
+	return retrieveDevicesForResult(result)
+}
+
+// GetConnectedBluetoothDevices returns a list of all connected bluetooth devices
+// bluetoothctl devices Connected
+func GetConnectedBluetoothDevices() ([]BluetoothDevice, error) {
+	result, err := util.ExecCommand(
+		"bluetoothctl",
+		"devices",
+		"Connected",
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return retrieveDevicesForResult(result)
+}
+
+// GetPairedBluetoothDevices returns a list of all paired bluetooth devices
+func GetPairedBluetoothDevices() ([]BluetoothDevice, error) {
+	result, err := util.ExecCommand(
+		"bluetoothctl",
+		"devices",
+		"Paired",
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return retrieveDevicesForResult(result)
+}
+
+func getFullInfoDevices(simpleDeviceInfos []simpleDeviceInfo) (devices []BluetoothDevice, err error) {
+	for _, deviceId := range simpleDeviceInfos {
+		info, err := GetBluetoothDeviceInfo(deviceId)
+		if err != nil {
+			return nil, err
+		}
+		devices = append(devices, info)
+	}
+	return devices, err
+}
+
+func parseSimpleDeviceList(result string) ([]simpleDeviceInfo, error) {
 	resultLines := strings.Split(result, "\n")
-	devices := make([]BluetoothDevice, 0)
+	devices := make([]simpleDeviceInfo, 0)
 	for _, line := range resultLines {
 		if strings.Contains(line, "Device ") {
 			parts := strings.Split(line, " ")
-			device := BluetoothDevice{
+			device := simpleDeviceInfo{
 				Address: parts[1],
 				Name:    parts[2],
 			}
@@ -76,4 +141,98 @@ func GetBluetoothDevices() ([]BluetoothDevice, error) {
 	}
 
 	return devices, nil
+}
+
+func GetBluetoothDeviceInfo(device simpleDeviceInfo) (BluetoothDevice, error) {
+	result, err := util.ExecCommand(
+		"bluetoothctl",
+		"info",
+		device.Address,
+	)
+	if err != nil {
+		return BluetoothDevice{}, err
+	}
+
+	return parseBluetoothDeviceInfo(result)
+}
+
+// parseBluetoothDeviceInfo parses the output of "bluetoothctl info <address>"
+func parseBluetoothDeviceInfo(input string) (result BluetoothDevice, err error) {
+	lines := strings.Split(input, "\n")
+
+	result.UUIDs = make(map[string]uuid.UUID)
+
+	for _, line := range lines {
+		if strings.Contains(line, "Device ") {
+			parts := strings.Split(input, " ")
+			result.Address = parts[1]
+		} else if strings.Contains(line, "Name: ") {
+			result.Name = strings.TrimSpace(strings.ReplaceAll(line, "Name: ", ""))
+		} else if strings.Contains(line, "Alias: ") {
+			result.Alias = strings.TrimSpace(strings.ReplaceAll(line, "Alias: ", ""))
+		} else if strings.Contains(line, "Class: ") {
+			result.Class = strings.TrimSpace(strings.ReplaceAll(line, "Class: ", ""))
+		} else if strings.Contains(line, "Icon: ") {
+			result.Icon = strings.TrimSpace(strings.ReplaceAll(line, "Icon: ", ""))
+		} else if strings.Contains(line, "Paired: ") {
+			result.Paired = strings.Contains(line, "yes")
+		} else if strings.Contains(line, "Bonded: ") {
+			result.Bonded = strings.Contains(line, "yes")
+		} else if strings.Contains(line, "Trusted: ") {
+			result.Trusted = strings.Contains(line, "yes")
+		} else if strings.Contains(line, "Blocked: ") {
+			result.Blocked = strings.Contains(line, "yes")
+		} else if strings.Contains(line, "Connected: ") {
+			result.Connected = strings.Contains(line, "yes")
+		} else if strings.Contains(line, "LegacyPairing: ") {
+			result.LegacyPairing = strings.Contains(line, "yes")
+		} else if strings.Contains(line, "UUID: ") {
+			indexOfTitle := strings.Index(line, "UUID: ")
+			value := strings.TrimSpace(line[indexOfTitle+5:])
+			indexOfLastBracket := strings.LastIndex(value, "(")
+			key := strings.TrimSpace(value[:indexOfLastBracket])
+			uuidString := strings.TrimSpace(value[indexOfLastBracket+1 : len(value)-1])
+
+			uuidValue, err := uuid.Parse(uuidString)
+			if err != nil {
+				return result, err
+			}
+			result.UUIDs[key] = uuidValue
+		} else if strings.Contains(line, "Battery Percentage: ") {
+			parts := strings.Split(line, " ")
+			batteryPercentageString := parts[2]
+			batteryPercentage, err := strconv.ParseInt(batteryPercentageString, 0, 64)
+			if err != nil {
+				return result, err
+			}
+			result.BatteryPercentage = &batteryPercentage
+		}
+	}
+
+	return result, nil
+}
+
+func retrieveDevicesForResult(result string) ([]BluetoothDevice, error) {
+	deviceIdList, err := parseSimpleDeviceList(result)
+	if err != nil {
+		return nil, err
+	}
+
+	return getFullInfoDevices(deviceIdList)
+}
+
+// SetBluetoothScan enables or disables bluetooth scanning
+func SetBluetoothScan(enable bool) error {
+	arg := "off"
+	if enable {
+		arg = "on"
+	}
+
+	_, err := util.ExecCommand(
+		"bluetoothctl",
+		"scan",
+		arg,
+	)
+
+	return err
 }

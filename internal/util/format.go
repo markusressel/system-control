8d097fc93b5c14827d814cb26d9e320acc81ab90
Markus Ressel <mail@markusressel.de>
2024-10-01 22:20:39 +0200

    docs

--- internal/util/format.go
+++ internal/util/format.go
@@ -2,6 +2,8 @@ package util
 
 import "strings"
 
+// ReplacePlaceholders replaces placeholders in a template string with the values from a map.
+// Placeholders are defined by a percent sign followed by the key and another percent sign, e.g. %key%.
 func ReplacePlaceholders(template string, placeholders map[string]string) string {
 	result := template
 	for key, value := range placeholders {

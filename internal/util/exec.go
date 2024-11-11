4da883e5bbbec1104f1891d2b30a8acf21773d63
Markus Ressel <mail@markusressel.de>
2024-11-11 02:38:17 +0100

    refactor

--- internal/util/exec.go
+++ internal/util/exec.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -47,48 +46,4 @@ func ExecCommandOneshot(timeout time.Duration, command string, args ...string) e
 		return err
 	}
 	return nil
-}
-
-const DefaultColumnHeaderRegexPattern = "\\S+\\s*"
-
-// ParseTable attempts to parse the given input string as a table, converting each row into
-// a slice of structs using the provided producer function.
-//
-// The first line is expected to be the header line, all following lines are expected to be data lines.
-// The header line is used to determine the number of columns, their with and order.
-// The cellSeparator is a regex that is used to separate columns within the header lines. The regex match
-// must include any whitespace that follows the column title.
-//
-// The producer function is expected to map the values of a row into a struct of type T.
-func ParseTable[T any](input string, cellSeparator string, producer func(row []string) T) ([]T, error) {
-	result := make([]T, 0)
-
-	lines := strings.Split(input, "\n")
-	if len(lines) < 2 {
-		return nil, fmt.Errorf("invalid table format")
-	}
-
-	headerCellRegex := regexp.MustCompile(cellSeparator)
-	header := headerCellRegex.FindAllString(lines[0], -1)
-	if len(header) < 2 {
-		return nil, fmt.Errorf("invalid table format")
-	}
-
-	for i := 1; i < len(lines); i++ {
-		row := make([]string, 0)
-		currentLine := lines[i]
-		startIdx := 0
-		for i := 0; i < len(header); i++ {
-			endIdx := startIdx + len(header[i])
-			columnValue := SubstringRunes(currentLine, startIdx, endIdx)
-			row = append(row, columnValue)
-			startIdx = endIdx
-		}
-		if len(row) < 2 {
-			return nil, fmt.Errorf("invalid table format")
-		}
-		result = append(result, producer(row))
-	}
-
-	return result, nil
 }
--- internal/util/tui.go
+++ internal/util/tui.go
@@ -23,6 +23,7 @@ import (
 	"math"
 	"os"
 	"regexp"
+	"strings"
 	"text/tabwriter"
 )
 
@@ -72,4 +73,48 @@ func PrintFormattedTableOrdered(title string, properties *orderedmap.OrderedMap[
 		_, _ = fmt.Fprintf(w, "  %s:\t%s\t\n", key, value)
 	}
 	_ = w.Flush()
+}
+
+const DefaultColumnHeaderRegexPattern = "\\S+\\s*"
+
+// ParseTable attempts to parse the given input string as a table, converting each row into
+// a slice of structs using the provided producer function.
+//
+// The first line is expected to be the header line, all following lines are expected to be data lines.
+// The header line is used to determine the number of columns, their with and order.
+// The cellSeparator is a regex that is used to separate columns within the header lines. The regex match
+// must include any whitespace that follows the column title.
+//
+// The producer function is expected to map the values of a row into a struct of type T.
+func ParseTable[T any](input string, cellSeparator string, producer func(row []string) T) ([]T, error) {
+	result := make([]T, 0)
+
+	lines := strings.Split(input, "\n")
+	if len(lines) < 2 {
+		return nil, fmt.Errorf("invalid table format")
+	}
+
+	headerCellRegex := regexp.MustCompile(cellSeparator)
+	header := headerCellRegex.FindAllString(lines[0], -1)
+	if len(header) < 2 {
+		return nil, fmt.Errorf("invalid table format")
+	}
+
+	for i := 1; i < len(lines); i++ {
+		row := make([]string, 0)
+		currentLine := lines[i]
+		startIdx := 0
+		for i := 0; i < len(header); i++ {
+			endIdx := startIdx + len(header[i])
+			columnValue := SubstringRunes(currentLine, startIdx, endIdx)
+			row = append(row, columnValue)
+			startIdx = endIdx
+		}
+		if len(row) < 2 {
+			return nil, fmt.Errorf("invalid table format")
+		}
+		result = append(result, producer(row))
+	}
+
+	return result, nil
 }
